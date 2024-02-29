6a2114fd8d5d856c5b092dd0f6977a4c2ca6a2d1
Matt Villa <matthewvilla805@gmail.com>
2024-02-28 23:59:15 -0800

    Add salary-related API endpoints and error handling

--- controllers/offerController.ts
+++ controllers/offerController.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import Offer from "../models/Offer";
+import { Request } from "express";
 
 const router = express.Router();
+const standardErrorMessage = "An unexpected error occured.";
 
 router.get("/", async (req, res) => {
   try {
@@ -25,9 +27,104 @@ router.post("/", async (req, res) => {
     res.json({ newOfferId: newOfferId });
   } catch (err) {
     console.error(err);
-    const errorMessage =
-      err instanceof Error ? err.message : "An unexpected error occurred";
-    res.status(500).send(errorMessage);
+    res.status(500).send(standardErrorMessage);
+  }
+});
+
+router.post("/salaries/groups", async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(standardErrorMessage);
+  }
+});
+
+/**
+ * Retrieves the maximum salary from the request query parameters.
+ *
+ * @param req - The request object containing the query parameters.
+ * @returns The maximum salary as a number, or undefined if not provided or invalid.
+ * @throws Error if the provided maximum salary is not a valid integer.
+ */
+function getMax(req: Request): number | undefined {
+  let max: number | undefined;
+  if (req.query.max && typeof req.query.max === "string") {
+    max = parseInt(req.query.max);
+  } else if (
+    req.query.max &&
+    typeof req.query.max !== "string" &&
+    typeof req.query.max !== "number"
+  ) {
+    throw new Error("Max salary required to be an integer");
+  } else if (typeof req.query.max === "number") {
+    max = req.query.max;
+  } else {
+    max = undefined;
+  }
+  return max;
+}
+
+router.get("/salaries", async (req, res) => {
+  try {
+    const max = getMax(req);
+    const salaries = await Offer.getAllSalaries(max);
+    res.json(salaries);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(standardErrorMessage);
+  }
+});
+
+router.get("/salaries/state/:state", async (req, res) => {
+  try {
+    const state = req.params.state;
+    const max = getMax(req);
+    const salaries = await Offer.getSalariesByState(state, max);
+    res.json({ state: state, salaries: salaries });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(standardErrorMessage);
+  }
+});
+
+router.get("/salaries/average", async (req, res) => {
+  try {
+    const state = req.query.state;
+    if (state && typeof state !== "string") {
+      res.status(400).send("State must be a string");
+      return;
+    }
+
+    const max = getMax(req);
+    let salaries;
+    if (state) {
+      salaries = await Offer.getSalariesByState(state, max);
+    } else {
+      salaries = await Offer.getAllSalaries(max);
+    }
+    const average = Math.round(
+      salaries.reduce((a, b) => a + b, 0) / salaries.length
+    );
+
+    if (state) {
+      res.json({ state, average });
+    } else {
+      res.json({ average });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(standardErrorMessage);
+  }
+});
+
+router.get("/salaries/groups", async (req, res) => {
+  try {
+    const salariesByGroup = await Offer.getSalaryGroups();
+
+    res.json({ salariesByGroup });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(standardErrorMessage);
   }
 });
 
--- models/Offer.ts
+++ models/Offer.ts
@@ -1,25 +1,30 @@
 import db from "../db";
+import { RowDataPacket } from "mysql2";
+
+interface SalaryRecord {
+  salary: number;
+}
 
 class Offer {
   static async getAll(): Promise<any> {
     const offers = await db.query(`
     SELECT 
-    Offers.idOffer,
-    Offers.userName,
-    Offers.offerDate,
-    IFNULL(CASE WHEN Offers.personalProject = 1 THEN 'Yes' ELSE 'No' END, 'N/A') AS personalProject,
-    IFNULL(CASE WHEN Offers.returnship = 1 THEN 'Yes' ELSE 'No' END, 'N/A') AS returnship,
-    Offers.timeInProgram,
-    Offers.salary,
-    Offers.gpa,
-    IFNULL(CASE WHEN Offers.swePosition = 1 THEN 'Yes' ELSE 'No' END, 'N/A') AS swePosition,
-    IFNULL(CASE WHEN Offers.bigTechOffer = 1 THEN 'Yes' ELSE 'No' END, 'N/A') AS bigTechOffer,
-    Offers.ageOfCandidate,
-    IFNULL(OfferSources.type, 'N/A') AS offerSourceType,
-    IFNULL(OfficeLocations.location, 'N/A') AS officeLocation,
-    IFNULL(WorkArrangements.arrangement, 'N/A') AS workArrangement,
-    IFNULL(PriorExperiences.experienceType, 'N/A') AS priorExperienceType,
-    IFNULL(PreviousDegrees.degreeType, 'N/A') AS previousDegreeType
+      Offers.idOffer,
+      Offers.userName,
+      Offers.offerDate,
+      IFNULL(CASE WHEN Offers.personalProject = 1 THEN 'Yes' ELSE 'No' END, 'N/A') AS personalProject,
+      IFNULL(CASE WHEN Offers.returnship = 1 THEN 'Yes' ELSE 'No' END, 'N/A') AS returnship,
+      Offers.timeInProgram,
+      Offers.salary,
+      Offers.gpa,
+      IFNULL(CASE WHEN Offers.swePosition = 1 THEN 'Yes' ELSE 'No' END, 'N/A') AS swePosition,
+      IFNULL(CASE WHEN Offers.bigTechOffer = 1 THEN 'Yes' ELSE 'No' END, 'N/A') AS bigTechOffer,
+      Offers.ageOfCandidate,
+      IFNULL(OfferSources.type, 'N/A') AS offerSourceType,
+      IFNULL(OfficeLocations.location, 'N/A') AS officeLocation,
+      IFNULL(WorkArrangements.arrangement, 'N/A') AS workArrangement,
+      IFNULL(PriorExperiences.experienceType, 'N/A') AS priorExperienceType,
+      IFNULL(PreviousDegrees.degreeType, 'N/A') AS previousDegreeType
     FROM 
         Offers
     LEFT JOIN 
@@ -34,6 +39,46 @@ class Offer {
         PreviousDegrees ON Offers.idPreviousDegree = PreviousDegrees.idPreviousDegree;
     `);
     return offers[0];
+  }
+
+  static async getAllSalaries(max?: number) {
+    let sql = "SELECT salary FROM Offers";
+    let params: number[] = [];
+
+    if (max !== undefined) {
+      sql += " WHERE salary < ?";
+      params.push(max);
+    }
+
+    const result = await db.query(sql, params);
+    const salaries = (result[0] as RowDataPacket[]).map(
+      (record: any) => record.salary
+    );
+
+    return salaries;
+  }
+
+  static async getSalariesByState(state: string, max?: number) {
+    let sql = "SELECT salary FROM Offers ";
+    let params: (number | string)[] = [];
+
+    if (max !== undefined) {
+      sql += "WHERE salary < ? AND ";
+      params.push(max);
+    } else {
+      sql += "WHERE ";
+    }
+
+    sql += `
+    idOfficeLocation = (SELECT idOfficeLocation FROM officeLocations WHERE location = ?)`;
+    params.push(state);
+
+    const result = await db.query(sql, params);
+    const salaries = (result[0] as RowDataPacket[]).map(
+      (record: any) => record.salary
+    );
+
+    return salaries;
   }
 
   static async create(offerData: {
@@ -94,6 +139,59 @@ class Offer {
     } else {
       throw new Error("Failed to insert the offer.");
     }
+  }
+
+  static async getSalaryGroups(): Promise<any> {
+    const salaryGroups = await db.query(`
+    SELECT 
+      salary_ranges.salary_range,
+      COUNT(subquery.salary) AS count
+    FROM 
+      (
+        SELECT '20k to 40k' AS salary_range
+        UNION SELECT '40k to 60k'
+        UNION SELECT '60k to 80k'
+        UNION SELECT '80k to 100k'
+        UNION SELECT '100k to 120k'
+        UNION SELECT '120k to 140k'
+        UNION SELECT '140k to 160k'
+        UNION SELECT '160k to 180k'
+        UNION SELECT '180k to 200k'
+        UNION SELECT '200k+'
+      ) AS salary_ranges
+    LEFT JOIN 
+      (SELECT salary FROM offers) AS subquery 
+    ON 
+      CASE 
+        WHEN subquery.salary >= 20000 AND subquery.salary < 40000 THEN '20k to 40k'
+        WHEN subquery.salary >= 40000 AND subquery.salary < 60000 THEN '40k to 60k'
+        WHEN subquery.salary >= 60000 AND subquery.salary < 80000 THEN '60k to 80k'
+        WHEN subquery.salary >= 80000 AND subquery.salary < 100000 THEN '80k to 100k'
+        WHEN subquery.salary >= 100000 AND subquery.salary < 120000 THEN '100k to 120k'
+        WHEN subquery.salary >= 120000 AND subquery.salary < 140000 THEN '120k to 140k'
+        WHEN subquery.salary >= 140000 AND subquery.salary < 160000 THEN '140k to 160k'
+        WHEN subquery.salary >= 160000 AND subquery.salary < 180000 THEN '160k to 180k'
+        WHEN subquery.salary >= 180000 AND subquery.salary < 200000 THEN '180k to 200k'
+        ELSE '200k+'
+      END = salary_ranges.salary_range
+    GROUP BY 
+      salary_ranges.salary_range
+    ORDER BY
+      CASE 
+        WHEN salary_ranges.salary_range = '20k to 40k' THEN 1
+        WHEN salary_ranges.salary_range = '40k to 60k' THEN 2
+        WHEN salary_ranges.salary_range = '60k to 80k' THEN 3
+        WHEN salary_ranges.salary_range = '80k to 100k' THEN 4
+        WHEN salary_ranges.salary_range = '100k to 120k' THEN 5
+        WHEN salary_ranges.salary_range = '120k to 140k' THEN 6
+        WHEN salary_ranges.salary_range = '140k to 160k' THEN 7
+        WHEN salary_ranges.salary_range = '160k to 180k' THEN 8
+        WHEN salary_ranges.salary_range = '180k to 200k' THEN 9
+        ELSE 10
+      END;
+    `);
+
+    return salaryGroups[0];
   }
 }
 
